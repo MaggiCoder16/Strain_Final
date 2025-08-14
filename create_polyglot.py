53466b1f10b55ebb4df4fdfcce34c7f3d3cc19f4
maggicoder <156202777+MaggiCoder16@users.noreply.github.com>
2025-08-14 16:46:44 +0530

    Update create_polyglot.py

--- create_polyglot.py
+++ create_polyglot.py
@@ -105,4 +105,4 @@ def build_book_file(pgn_path, book_path):
     book.save_as_polyglot(book_path)
 
 if __name__ == "__main__":
-    build_book_file("filtered_960_bots_2200plus.pgn", "book.bin")
+    build_book_file("filtered_960_bots_2400plus.pgn", "book.bin")
